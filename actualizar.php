3727c170e0abccae714898a5a9322e3e5b38fa84
HugoIP <bairon.myh@gmail.com>
2018-11-21 07:22:14 -0600

    Update first function forms id

--- actualizar.php
+++ actualizar.php
@@ -118,13 +118,13 @@ $contador=0;
 while($misdatos = mysqli_fetch_assoc($resultado)){ $contador++;?>
 <tr>
   <td>
-  <td id=<?php echo ("NAME-"+$misdatos["serviceNum"]); ?>> <?php echo $misdatos["serviceName"]; ?></td>
-  <td id=<?php echo ("BARC-"+$misdatos["serviceNum"]); ?>><?php echo $misdatos["barCode"]; ?></td>
-  <td id=<?php echo ("SNUM-"+$misdatos["serviceNum"]); ?>><?php echo $misdatos["serviceNum"]; ?></td>
-  <td id=<?php echo ("SPAY-"+$misdatos["serviceNum"]); ?>><?php echo $misdatos["pay"]; ?></td>
-  <td id=<?php echo ("LIMI-"+$misdatos["serviceNum"]); ?>><?php echo $misdatos["limitPay"]; ?></td>
-  <td id=<?php echo ("ORDE-"+$misdatos["serviceNum"]); ?>><?php echo $misdatos["orderGrup"]; ?></td>
-  <td id=<?php echo ("STAT-"+$misdatos["serviceNum"]); ?>><?php echo $misdatos["texStatus"]; ?></td>
+  <td id=<?php echo ("NAME".$misdatos["serviceNum"]); ?>> <?php echo $misdatos["serviceName"]; ?></td>
+  <td id=<?php echo ("BARC".$misdatos["serviceNum"]); ?>><?php echo $misdatos["barCode"]; ?></td>
+  <td id=<?php echo ("SNUM".$misdatos["serviceNum"]); ?>><?php echo $misdatos["serviceNum"]; ?></td>
+  <td id=<?php echo ("SPAY".$misdatos["serviceNum"]); ?>><?php echo $misdatos["pay"]; ?></td>
+  <td id=<?php echo ("LIMI".$misdatos["serviceNum"]); ?>><?php echo $misdatos["limitPay"]; ?></td>
+  <td id=<?php echo ("ORDE".$misdatos["serviceNum"]); ?>><?php echo $misdatos["orderGrup"]; ?></td>
+  <td id=<?php echo ("STAT".$misdatos["serviceNum"]); ?>><?php echo $misdatos["texStatus"]; ?></td>
   </tr>
           
 <?php }?>          
