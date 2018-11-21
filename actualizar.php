e93235bcc7a886d9aae171cc660026cf9926617e
HugoIP <bairon.myh@gmail.com>
2018-11-21 06:23:28 -0600

    Add new functions find

--- actualizar.php
+++ actualizar.php
@@ -1,20 +1,3 @@
-<?php
-include "db.php";
-
-if(isset($_POST["btnActualizar"])){
-	$con = connect();
-	$serviceName=$_POST['serviceName'];
-	$barCode=$_POST['barCode'];
-	$serviceNum=$_POST['serviceNum'];
-  $pay=$_POST['pay'];
-  $orderGrup=$_POST['orderGrup'];
-  $dateIntro=date("Y-m-d");
-	
-	$con->query("insert into Servicios (serviceName, barCode, serviceNum, pay, dateIntro,texStatus, orderGrup) value ('$serviceName', '$barCode', '$serviceNum','$pay','$dateIntro','$dateIntro','$texStatus', '$orderGrup')");
-	header("Location: index.php?option=ok");
-}
-
-?>
 <!doctype html>
 <html lang="es">
   <head>
@@ -23,39 +6,28 @@ if(isset($_POST["btnActualizar"])){
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="favicon.ico">
+<title>ACtualizar Recibos</title>
+<script src="http://code.jquery.com/jquery-2.1.4.min.js" type="text/javascript"></script>
+<!-- Bootstrap core CSS -->
+<link href="dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- Custom styles for this template -->
+<link href="assets/css/sticky-footer-navbar.css" rel="stylesheet">
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css" />
+<script type="text/javascript">
+$(document).ready(function () {
+   (function($) {
+       $('#FiltrarContenido').keyup(function () {
+            var getServiceNum = String($(this).val()).substring(2,13);
 
-    <title>Actualizar Recibos - vNova Internet</title>
-
-    <script src="http://code.jquery.com/jquery-2.1.4.min.js" type="text/javascript"></script>
-    <!-- Custom styles for this template -->
-    <link href="assets/css/sticky-footer-navbar.css" rel="stylesheet">
-    <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css" />
-    <script type="text/javascript">
-    $(document).ready(function () {
-       (function($) {
-           $('#ObtenerDatos').keyup(function () {
-                var ValorBusqueda = $(this).val();
-                alert( $(this).val());
-                $.ajax({
-                    type:'POST',
-                    url: 'consultarData.php',
-                    data: ValorBusqueda,
-                    datatype: 'json',
+            var ValorBusqueda = new RegExp(getServiceNum, 'i');
+            $('.BusquedaRapida tr').hide();
+             $('.BusquedaRapida tr').filter(function () {
+                return ValorBusqueda.test($(this).text());
+              }).show();
                 })
-
-                // Compruebo si me esta trayendo los valores
-
-                .done(function(data){
-
-                    console.log(data);
-                    var datos2 = JSON.parse(data);
-                    console.log(datos2);
-
-                })
-                    })
-          }(jQuery));
-    });
-    </script> 
+      }(jQuery));
+});
+</script> 
   </head>
 
   <body>
@@ -68,11 +40,9 @@ if(isset($_POST["btnActualizar"])){
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
-
           <ul class="navbar-nav mr-auto">
-          
+           
             <li class="nav-item active">
-              <a class="nav-link" href="actualizar.php">Actualizar <span class="sr-only">(current)</span></a>
               <a class="nav-link" href="registro.php">Registrar <span class="sr-only">(current)</span></a>
             </li>  
                   
@@ -88,58 +58,64 @@ if(isset($_POST["btnActualizar"])){
     <!-- Begin page content -->
 
 <div class="container">
- <h1 class="mt-5">Registrar Recibo</h1>
+ <h1 class="mt-5">Busqueda</h1>
  <hr>
+<?php
+if(isset($_GET["option"])){?>
+ <div class="alert alert-success" role="alert">
+  <strong>Hecho!</strong> El registro ha sido guardado con exito.
+</div>
+<?php }?>
+<div class="row">
+  <div class="col-12 col-md-12">
 
+   <!-- Contenido -->    
+			
 
 <div class="input-group mb-3">
   <div class="input-group-prepend">
-    <span class="input-group-text" id="basic-addon1">Buscar BarCode</span>
+    <span class="input-group-text" id="basic-addon1">Buscar</span>
   </div>
-  <input id="ObtenerDatos" type="text" class="form-control" placeholder="Codigo de barras" aria-label="Alumno" aria-describedby="basic-addon1">
+  <input id="FiltrarContenido" type="text" class="form-control" placeholder="Ingrese Nombre del servicio" aria-label="Alumno" aria-describedby="basic-addon1">
+</div>
+	    <table class="table table-hover">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Codigo de barras</th>
+            <th>Servicio</th>            
+            <th>Monto</th>
+            <th>Grupo</th>
+            <th>Entregado</th>
+          </tr>
+        </thead>
+        <tbody class="BusquedaRapida">
+<?php
+include "db.php";
+$con = connect();
+$consulta = "SELECT * FROM Servicios";
+$resultado = mysqli_query($con , $consulta);
+$contador=0;
+
+while($misdatos = mysqli_fetch_assoc($resultado)){ $contador++;?>
+<tr>
+  <td><?php echo $misdatos["serviceName"]; ?></td>
+  <td><?php echo $misdatos["barCode"]; ?></td>
+  <td><?php echo $misdatos["serviceNum"]; ?></td>
+  <td><?php echo $misdatos["pay"]; ?></td>
+  <td><?php echo $misdatos["orderGrup"]; ?></td>
+  <td><?php echo $misdatos["texStatus"]; ?></td>
+  </tr>
+          
+<?php }?>          
+
+</tbody>
+      </table>		
+<!-- Fin Contenido --> 
 </div>
 
 
-<div class="row">
-  <div class="col-12 col-md-6">
 
-   <!-- Contenido --> 
-
-<form id="frmLogin" action="" method="post">
-  <fieldset>
-        <div class="form-group">
-    <label for="serviceName">Nombre:</label>
-    <input required type="text" class="form-control" name="apenom" placeholder="Nombre del servicio" value="">
- 	   </div>
-        <div class="form-group">
-    <label for="barCode">Codigo de barras:</label>
-    <input required class="form-control" type="text" name="barCode"  placeholder="Codigo de barras" value="">
- 	   </div>
-
-        <div class="form-group">
-    <label for="serviceNum">Numero de servicio:</label>
-    <input required class="form-control" type="serviceNum" name="serviceNum"  placeholder="Numero de servicio 12 d" value="">
- 	   </div>
-       
-       <div class="form-group">
-    <label for="pay">Cantidad:</label>
-    <input required class="form-control" type="pay" name="pay"  placeholder="Cantidad a pagar" value="">
-     </div>
-     <div class="form-group">
-    <label for="orderGrup">Cantidad:</label>
-    <input required class="form-control" type="orderGrup" name="orderGrup"  placeholder="Grupo" value="">
-     </div>
-       
-       
-    <input type="hidden" name="btnActualizar" value="v">
-<input class="btn btn-primary" type="submit" value="Actualizar recibo">
-             
-  </fieldset>
-
-</form>
-
- <!-- Fin Contenido --> 
-</div>
 </div><!-- Fin row -->
 
 
@@ -152,10 +128,10 @@ if(isset($_POST["btnActualizar"])){
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
- 
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
     <script src="assets/js/vendor/popper.min.js"></script>
     <script src="dist/js/bootstrap.min.js"></script>
+    
   </body>
 </html>
