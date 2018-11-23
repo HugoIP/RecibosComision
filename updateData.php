9f157ac1400b53f38d234594105e4ccb4ef0a4d4
HugoIP <bairon.myh@gmail.com>
2018-11-23 01:46:29 -0600

    Update php updateData

--- updateData.php
+++ updateData.php
@@ -3,7 +3,6 @@ include "db.php";
 $msg="NoProcess";
 echo "init";
 if(isset($_GET['barCode'])){
-  echo "in _";
 	$con = connect();
 
   $serviceNum=$_GET['serviceNum'];
@@ -12,26 +11,11 @@ if(isset($_GET['barCode'])){
   $texStatus=$_GET['texStatus'];
   $orderGrup=$_GET['orderGrup'];
   $limitPay=$_GET['limitPay'];
-echo "get";
-	$consulta="UPDATE servicios SET barCode='$barCode', dateIntro='$dateIntro',texStatus='$texStatus', orderGrup='$orderGrup', limitPay='$limitPay' WHERE serviceNum='$serviceNum'";
+echo " get ";
+	$consulta="UPDATE Servicios SET barCode='$barCode', dateIntro='$dateIntro',texStatus='$texStatus', orderGrup='$orderGrup', limitPay='$limitPay' WHERE serviceNum='$serviceNum'";
   mysqli_query($con , $consulta);
-  echo "update";
-  $sql = "SELECT * FROM servicios WHERE serviceNum='$serviceNum'";
-  $result = $conn->query($sql);
-  echo "select";
-
-  if ($result->num_rows > 0) {
-      while($row = $result->fetch_assoc()) {  
-  //name, telefono, email, Dia, Mes, UltimoCelebrado
-          $barCode = $row['barCode'];
-        
-      }
-      $msg= $barCode;
-  }
-  else
-  {
-    $msg= "Error";
-  }
+  echo " get 2";
+  $msg="Ok";
 }
 echo $msg;
   
