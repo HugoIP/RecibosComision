391e2471e79177102eaa0335a6e67c36ea8d1300
HugoIP <bairon.myh@gmail.com>
2019-11-17 09:29:57 -0600

    Add ciclos page manager jquery

--- updateCiclos.php
+++ updateCiclos.php
@@ -2,13 +2,13 @@
 error_reporting(E_ALL); 
 ini_set("display_errors", 1); 
 include "db.php";
+$ciclo=$_GET['ciclo'];
 if(isset($_GET['ciclo'])){
 	$con = connect();
   //if($_GET['status']=="open")
   //{
     //var params = {ciclo:getCiclo, poblacion:getPoblacion, importe:getImporte,total:getTotal, operador:getOperador, paq:getPaq, paq:getPaq, grupo:getGrupo, ordinal:getOrdinal, status:getStatus};
     $dateInit= date("Y-m-d H:i:s");
-    $ciclo=$_GET['ciclo'];
     $poblacion=$_GET['poblacion'];
     $importe=$_GET['importe'];
     $total=$_GET['total'];
@@ -18,7 +18,7 @@ if(isset($_GET['ciclo'])){
     $ordinal=$_GET['ordinal'];
     $status=$_GET['status'];
 
-    $consulta="INSERT INTO `Ciclos`(`fechaCiclo`, `ciclo`, `poblacion`, `importe`, `totalRecibos`, `operador`, `paquetesTam`, `groupOrder`, `ordinal`, `status`) VALUES ($dateInit,$ciclo,$poblacion,$importe,$total,$operador,$paq,$grupo,$ordinal,$status)";
+    $consulta="INSERT INTO Ciclos (fechaCiclo, ciclo, poblacion, importe, totalRecibos, operador, paquetesTam, groupOrder, ordinal, status) VALUES ($dateInit,$ciclo,$poblacion,$importe,$total,$operador,$paq,$grupo,$ordinal,$status)";
     //$consulta="UPDATE Servicios SET barCode='$barCode', limitPay=$limitPay, dateIntro=$dateIntro, texStatus=$texStatus, orderGrup=$orderGrup WHERE serviceNum=$serviceNum";
     mysqli_query($con,$consulta);
     mysqli_close($con);
@@ -27,7 +27,7 @@ if(isset($_GET['ciclo'])){
  
   mysqli_close($con);
   $con = connect();
-   $result = mysqli_query($con,"SELECT * FROM Servicios WHERE serviceNum=$serviceNum");
+   $result = mysqli_query($con,"SELECT * FROM Ciclos WHERE ciclo=$ciclo");
 
  while($row = mysqli_fetch_array($result))
  {
